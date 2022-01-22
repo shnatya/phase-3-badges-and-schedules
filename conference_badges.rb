542bb846bab92e8e5343e662421de456502c6d56
Anastasia Shatokhina <shnatya@gmail.com>
2022-01-22 15:23:33 -0800

    commit tree d1db0a8c318b9a2912e7138d2787cb178663030e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(arr)
+    arr.map.with_index {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(arr)
+    batch_badge_creator(arr).map {|message| puts message}
+    assign_rooms(arr).map {|message| puts message}
+end
